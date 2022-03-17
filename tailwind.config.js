b27b9363b31cb688437b0d000e9420709d433632
Zain Shaikh <zainshaikh@zains-air.home>
2022-03-17 15:39:58 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm1: { max: "445px" },
+        sm2: { max: "360px" },
+        sm3: { max: "323px" },
+        sm4: { max: "400px" },
+        sm5: { max: "490px" },
+        md1: { max: "575px" },
+        md2: { max: "770px" },
+        md3: { max: "865px" },
+        md4: { max: "1170px" },
+      },
+    },
+  },
+  plugins: [],
+};
